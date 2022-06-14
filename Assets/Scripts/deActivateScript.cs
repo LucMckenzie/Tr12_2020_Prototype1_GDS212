8b89b25a260932faae3d4d4ee67b03f26ad95b30
Luc <lucmckenzie6@gmail.com>
2022-06-15 01:12:37 +1000

    ...

--- Assets/Scripts/deActivateScript.cs
+++ Assets/Scripts/deActivateScript.cs
@@ -2,16 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DeactivateScript : MonoBehaviour
+public class deActivateScript : MonoBehaviour
 {
+    
+    public GameObject hideText;
+
     // Start is called before the first frame update
     void Start()
     {
+        hideText = GameObject.Find("Canvas");
+        BroadcastMessage("hideText");
        
+        hideText.SetActive(false);
+        
     }
 
-    // Update is called once per frame
-    void Update()
+        // Update is called once per frame
+        void Update()
     {
         
     }
